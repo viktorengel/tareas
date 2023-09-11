e79ee142b17bfda20aaffc4f064be0b87599b94b
viktorengel <viktorengel@hotmail.com>
2023-09-10 20:22:09 -0500

    Modelo y migración vacias

--- resources/views/partes/index.blade.php
+++ resources/views/partes/index.blade.php
@@ -1,5 +1,40 @@
 @extends('plantilla')
 
 @section('content')
-    <h1>Hola mundo</h1>
+    <div class="container w-25 border p-4 mt-4">
+        <form action="" method="POST">
+            @csrf
+
+            {{-- @if (session('success'))
+                <h6 class="alert alert-success">{{ session('success') }}</h6>
+            @endif
+
+            @error('title')
+                <h6 class="alert alert-danger">{{ $message }}</h6>
+            @enderror --}}
+
+            <div class="mb-3">
+              <label for="title" class="form-label">Titulo de la tarea</label>
+              <input type="text" class="form-control" name="title">
+            </div>
+            <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+        </form>
+        <div>
+            {{-- @foreach ($todos as $todo)
+                <div class="row py-1">
+                    <div class="col-md-9 d-flex align-items-center">
+                        <a href="{{ route('todos-edit'), ['id' => $todo->id] }}">{{ $todo->title }}</a>
+                    </div>
+                    
+                    <div class="col-md-3 d-flex justify-content-end">
+                        <form action="{{ route('todos-destroy', [$todo->id]) }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger btn-sm">Eliminar</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach --}}
+        </div>
+    </div>
 @endsection
