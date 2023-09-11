721333b1c4ea16405684effb1c50c0a6e130c7f4
viktorengel <viktorengel@hotmail.com>
2023-09-10 20:36:31 -0500

    migración de la base de datos

--- database/migrations/2023_09_10_231342_create_tareas_table.php
+++ database/migrations/2023_09_10_231342_create_tareas_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('tareas', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
